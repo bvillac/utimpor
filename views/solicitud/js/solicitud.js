e5df9230ce4cb01e56ee4b5bb88aeb4384518646
root <root@prueba.utimpor>
2018-04-05 17:53:07 -0500

    datos java script

--- views/solicitud/js/solicitud.js
+++ views/solicitud/js/solicitud.js
@@ -69,22 +69,15 @@ function retornaFilaProducto(c, Grid, TbGtable, op) {
     var strFila = "";
     //var imgCol='<img class="btn-img" src="'+RutaImagenAccion+'/acciones/eliminar.png" >';
     strFila += '<td style="display:none; border:none;">' + Grid[c]['ids_reb'] + '</td>';
-    strFila += '<td>' + Grid[c]['ids_ban'] + '</td>';
+    strFila += '<td>' + Grid[c]['nom_ban'] + '</td>';
+    strFila += '<td>' + Grid[c]['tip_cta'] + '</td>';
     strFila += '<td>' + Grid[c]['num_cta'] + '</td>';
-    //strFila +='<td>'+ Grid[c]['pro_foto']+'</td>';
-    /*strFila += '<td>';
-    //Cuando hay Actualizacion de Datos
-    if(AccionTipo=="Update"){
-        var imgFoto=(Grid[c]['accion']=='edit')?Grid[c]['pro_foto']:limpiarFake($('#txth_producto_foto').val());
-        strFila += (Grid[c]['pro_foto'] != "") ? '<a data-title="'+ Grid[c]['pro_nombre'] +'" data-lightbox="image-'+Math.floor((Math.random() * 10) + 1)+'" href="' + $('#txth_imgfolder').val() + $('#txt_ftem_cedula').val()+'_'+$('#txth_ftem_id').val() + '/productos/' + imgFoto + '">Ver Foto</a>' : '<span class="label label-danger">No Tiene Foto</span>';
-    }else{
-        strFila += (Grid[c]['pro_foto'] != "") ? '<a data-title="'+ Grid[c]['pro_nombre'] +'" data-lightbox="image-'+Math.floor((Math.random() * 10) + 1)+'" href="' + $('#txth_imgfolder').val() + $('#txt_ftem_cedula').val() + '/productos/' + limpiarFake($('#txth_producto_foto').val()) + '">Ver Foto</a>' : '<span class="label label-danger">No Tiene Foto</span>';
-    }
-    strFila += '</td>';*/
+    strFila += '<td>' + Grid[c]['cre_ban'] + '</td>';
+    
     //strFila +='<td>'+ Grid[c]['pro_detalle_uso']+'</td>';
     strFila += '<td>';//¿Está seguro de eliminar este elemento?
     //strFila +='<a class="btn-img" onclick="eliminarItemsProducto('+Grid[c]['DEP_ID']+',\''+TbGtable+'\')" >'+imgCol+'</a>';
-    strFila += '<a onclick="eliminarItemsProducto(\'' + Grid[c]['pro_id'] + '\',\'' + TbGtable + '\')" ><span class="glyphicon glyphicon-trash"></span></a>';
+    strFila += '<a onclick="eliminarItemsBanco(\'' + Grid[c]['ids_reb'] + '\',\'' + TbGtable + '\')" ><span class="glyphicon glyphicon-trash"></span></a>';
     strFila += '</td>';
 
     if (op) {
@@ -96,8 +89,8 @@ function retornaFilaProducto(c, Grid, TbGtable, op) {
 // Recarga la Grid de Productos si Existe
 function recargarGridProducto() {
     var tGrid = 'TbG_Productos';
-    if (sessionStorage.dts_Producto) {
-        var arr_Grid = JSON.parse(sessionStorage.dts_Producto);
+    if (sessionStorage.dts_refeBancos) {
+        var arr_Grid = JSON.parse(sessionStorage.dts_refeBancos);
         if (arr_Grid.length > 0) {
             $('#' + tGrid + ' > tbody').html("");
             for (var i = 0; i < arr_Grid.length; i++) {
@@ -107,17 +100,18 @@ function recargarGridProducto() {
     }
 }
 
-function eliminarItemsProducto(val, TbGtable) {
+function eliminarItemsBanco(val, TbGtable) {
     var ids = "";
     //var count=0;
-    if (sessionStorage.dts_Producto) {
-        var Grid = JSON.parse(sessionStorage.dts_Producto);
+    if (sessionStorage.dts_refeBancos) {
+        var Grid = JSON.parse(sessionStorage.dts_refeBancos);
         if (Grid.length > 0) {
             $('#' + TbGtable + ' tr').each(function () {
                 ids = $(this).find("td").eq(0).html();
+                alert(ids);
                 if (ids == val) {
-                    var array = findAndRemove(Grid, 'pro_id', ids);
-                    sessionStorage.dts_Producto = JSON.stringify(array);
+                    var array = findAndRemove(Grid, 'ids_rep', ids);
+                    sessionStorage.dts_refeBancos = JSON.stringify(array);
                     //if (count==0){sessionStorage.removeItem('detalleGrid')}
                     $(this).remove();
                 }
@@ -130,9 +124,8 @@ function eliminarItemsProducto(val, TbGtable) {
 function agregarItemsBanco(opAccion) {
     var tGrid = 'TbG_Productos';
     var nombre = $('#txt_num_cta').val();
-    alert($('#cmb_banco option:selected').val());
     //Verifica que tenga nombre producto y tenga foto
-    if ($('#txt_num_cta').val() != "") {
+    if ($('#txt_num_cta').val() != "" &&  $('#cmb_banco option:selected').val() != 0) {
         var valor = $('#txt_num_cta').val();
         if (opAccion != "edit") {
             //*********   AGREGAR ITEMS *********
@@ -153,20 +146,16 @@ function agregarItemsBanco(opAccion) {
                     }
                 } else {
                     /*Agrego a la Sesion*/
-                    //Primer Items
-                    //arr_Grid[0] = objAntDep(retornarIndexArray(JSON.parse(sessionStorage.atc_antDeporte),'DEP_NOMBRE',valor),JSON.parse(sessionStorage.atc_antDeporte));
-                    arr_Grid[0] = objProducto(0);
-                    sessionStorage.dts_Producto = JSON.stringify(arr_Grid);
+                    //Primer Items                   
+                    sessionStorage.dts_refeBancos = JSON.stringify(arr_Grid);
                     addPrimerItemProducto(tGrid, arr_Grid, 0);
                     limpiarDetalle();
                 }
             } else {
                 //No existe la Session
                 //Primer Items
-                //arr_Grid[0] = objAntDep(retornarIndexArray(JSON.parse(sessionStorage.dts_Producto),'pro_nombre',valor),JSON.parse(sessionStorage.dts_Producto));
-                alert("llego 1");
                 arr_Grid[0] = objProducto(0);
-                sessionStorage.dts_Producto = JSON.stringify(arr_Grid);
+                sessionStorage.dts_refeBancos = JSON.stringify(arr_Grid);
                 addPrimerItemProducto(tGrid, arr_Grid, 0);
                 limpiarDetalle();
             }
@@ -174,44 +163,35 @@ function agregarItemsBanco(opAccion) {
             //data edicion
         }
     } else {
-        menssajeModal("NO_OK", "Error", "No Existe datos de Producto Y/o Imagen", "Información", "", "", "1");
+        showAlert('NO_OK', 'error', {"wtmessage": 'Debe seleccionar Información Requerida', "title":'Información'});
     }
 }
 
 function limpiarDetalle() {
     $('#txt_num_cta').val("");
     //$('#txt_detalle_uso').val("");
-    //$("#cmb_por_id option[value=3]").attr("selected", true);
+    $("#cmb_banco option[value=0]").attr("selected", true);
 //    $('#chk_envase').prop('checked', false);
 //    $('#chk_empaque').prop('checked', false);
 //    $('#chk_etiqueta').prop('checked', false);
 //    $('#chk_publicidad').prop('checked', false);
 //    $('#chk_otros').prop('checked', false);
     //Quita los Alertas
-    removeIco('#txt_prod_nombre');
-    removeIco('#txt_detalle_uso');
-    //$('#txt_producto_foto').fileinput('upload');
+    removeIco('#txt_num_cta');
   
 }
 
 function objProducto(indice) {
-    alert("llego 2");
     var rowGrid = new Object();
     rowGrid.ids_reb = indice;
     //rowGrid.ids_reb = 0;
-    //rowGrid.pro_nombre = $('#txt_prod_nombre').val();
-    //rowGrid.ids_ban  = $('#cmb_por_id option:selected').val();
-    //rowGrid.ids_ban  = $('#cmb_por_id option:selected').text();
     rowGrid.ids_ban =$('#cmb_banco option:selected').val();
-    rowGrid.tip_cta =$('#cmb_tip_cta option:selected').text();
+    rowGrid.nom_ban =$('#cmb_banco option:selected').text();
+    rowGrid.tip_cta =$('#cmb_tip_cta option:selected').val();
+    rowGrid.nom_cta =$('#cmb_tip_cta option:selected').text();
     rowGrid.num_cta = $('#txt_num_cta').val();
+    rowGrid.cre_ban ='P'; 
     //rowGrid.pro_envase = ($("#rbt_op1").prop("checked")) ? 1 : 0;
-    
-    /*rowGrid.pro_empaque = ($("#chk_empaque").prop("checked")) ? 1 : 0;
-    rowGrid.pro_etiqueta = ($("#chk_etiqueta").prop("checked")) ? 1 : 0;
-    rowGrid.pro_publicidad = ($("#chk_publicidad").prop("checked")) ? 1 : 0;
-    rowGrid.pro_otros = ($("#chk_otros").prop("checked")) ? 1 : 0;*/
-    
     rowGrid.accion = "new";
     return rowGrid;
 }
